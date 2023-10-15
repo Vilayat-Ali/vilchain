59491c02bc5a49c2cab486b8e881d7df0a258979
vilayat-ali <vilayatcodemysite@gmail.com>
2023-10-15 16:31:14 +0530

    added MDX support for next.js with app-router for docs and vite react for explorer for local/demo use

---
+++ docs/src/app/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Home from "@/markdown/Home.mdx";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+};
+
+export default page;
