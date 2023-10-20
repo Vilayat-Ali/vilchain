12d2546ce0278c3c12860b2361b6dbda1832348e
vilayat-ali <vilayatcodemysite@gmail.com>
2023-10-20 13:24:36 +0530

    added design for explorer

--- explorer/src/App.tsx
+++ explorer/src/App.tsx
@@ -1,7 +1,50 @@
-// lib
+import { Suspense, lazy, useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
-const App = () => {
-  return <div>App</div>;
-};
+import Dashboard from "./pages/Dashboard";
+import Loader from "./common/Loader";
+import routes from "./routes";
+
+const DefaultLayout = lazy(() => import("./layout/DefaultLayout"));
+
+function App() {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+  return loading ? (
+    <Loader />
+  ) : (
+    <>
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+        containerClassName="overflow-auto"
+      />
+      <Routes>
+        <Route element={<DefaultLayout />}>
+          <Route index element={<Dashboard />} />
+          {routes.map((routes, index) => {
+            const { path, component: Component } = routes;
+            return (
+              <Route
+                key={index}
+                path={path}
+                element={
+                  <Suspense fallback={<Loader />}>
+                    <Component />
+                  </Suspense>
+                }
+              />
+            );
+          })}
+        </Route>
+      </Routes>
+    </>
+  );
+}
 
 export default App;
--- explorer/src/main.tsx
+++ explorer/src/main.tsx
@@ -1,12 +1,14 @@
-// lib
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import './index.css';
+import './satoshi.css';
 
-// components
-import App from "./App.tsx";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
