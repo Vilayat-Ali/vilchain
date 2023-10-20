4eb576ec78903ac62ac8e80e82c8f2e8a4e7f9cf
vilayat-ali <vilayatcodemysite@gmail.com>
2023-10-20 17:52:43 +0530

    refactored explorer code

--- explorer/src/pages/Dashboard/index.tsx
+++ explorer/src/pages/Dashboard/index.tsx
@@ -1,31 +1,34 @@
-import CardFour from "../../components/CardFour.js";
-import CardOne from "../../components/CardOne.js";
-import CardThree from "../../components/CardThree.js";
-import CardTwo from "../../components/CardTwo.js";
+// components
+import Card from "../../components/Card";
 import ChartOne from "../../components/ChartOne.js";
-import ChartThree from "../../components/ChartThree.js";
 import ChartTwo from "../../components/ChartTwo.js";
 import ChatCard from "../../components/ChatCard.js";
-import MapOne from "../../components/MapOne.js";
-import TableOne from "../../components/TableOne.js";
+import TableOne from "../../components/Table.js";
 
 const ECommerce = () => {
   return (
     <>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-        <CardOne />
-        <CardTwo />
-        <CardThree />
-        <CardFour />
+        <Card title="Example" value="12.33" changePercentage={2.3} />
+        <Card title="Example" value="12.33" changePercentage={2.3} />
+        <Card title="Example" value="12.33" changePercentage={2.3} />
+        <Card title="Example" value="12.33" changePercentage={2.3} />
       </div>
 
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
         <ChartOne />
         <ChartTwo />
-        <ChartThree />
-        <MapOne />
         <div className="col-span-12 xl:col-span-8">
-          <TableOne />
+          <TableOne
+            title="Example"
+            heading={["name", "age", "sex", "age", "sex"]}
+            data={[
+              ["Ali", "20", "MALE", "20", "MALE"],
+              ["Ali", "20", "MALE", "20", "MALE"],
+              ["Ali", "20", "MALE", "20", "MALE"],
+              ["Ali", "20", "MALE", "20", "MALE"],
+            ]}
+          />
         </div>
         <ChatCard />
       </div>
