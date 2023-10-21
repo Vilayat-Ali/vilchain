690bdfd5860f978cd6d0256718e8b903de55ef46
vilayat-ali <vilayatcodemysite@gmail.com>
2023-10-21 13:20:37 +0530

    added code for utils and merkle root hash

--- chain/src/structure/src/block/mod.rs
+++ chain/src/structure/src/block/mod.rs
@@ -1,7 +1,10 @@
 use serde::{Deserialize, Serialize};
-use std::{collections::HashSet, default::Default, time::SystemTime};
+use std::{collections::HashMap, default::Default, time::SystemTime};
 
 use crate::txn::{publishable_txn::PublishableTransaction, Txn};
+use utils::hash;
+
+const BLOCK_TXN_SIZE: usize = 15;
 
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct BlockHeaders {
@@ -17,15 +20,64 @@ where
     Txn: PublishableTransaction + Serialize + Deserialize<'static> + std::fmt::Debug,
 {
     headers: BlockHeaders,
-    txns: HashSet<Txn>,
+    txns: HashMap<String, Txn>,
     timestamp: SystemTime,
+}
+
+impl Block {
+    fn compute_hash(hash_list: Vec<String>) -> String {
+        let window_size: usize = hash_list.len() / 2;
+        let have_left_over: bool = hash_list.len() as f64 % 2_f64 != 0_f64;
+        let mut stack: Vec<String> = Vec::with_capacity(window_size);
+
+        if have_left_over {
+            todo!()
+        } else {
+            for idx in 0..window_size {
+                let level_hash: String =
+                    hash::compute_hash(format!("{}{}", hash_list[idx], hash_list[idx + 1]));
+
+                stack.push(level_hash);
+            }
+        }
+
+        todo!()
+    }
+
+    fn compute_merkle_root_hash(&self) -> String {
+        let hash_list: Vec<String> = self
+            .txns
+            .iter()
+            .map(|txn| {
+                let txn_instance: Txn = txn.1.clone();
+                txn_instance.hash.unwrap()
+            })
+            .collect::<Vec<String>>();
+
+        Self::compute_hash(hash_list)
+    }
+
+    pub fn insert_txn(&mut self, txn: Txn) -> Result<(), &'static str> {
+        if self.txns.len() < BLOCK_TXN_SIZE {
+            // we are 100% sure that we will be having a hash as the txn struct implements PublishableTransaction trait
+            self.txns.insert(txn.hash.clone().unwrap(), txn);
+            self.compute_merkle_root_hash(); // updating merkle root hash
+            Ok(())
+        } else {
+            Err("Txn Limit reached")
+        }
+    }
+
+    pub fn get_txn_count(&self) -> usize {
+        self.txns.len()
+    }
 }
 
 impl Default for Block {
     fn default() -> Self {
         Self {
             headers: BlockHeaders::default(),
-            txns: HashSet::new(),
+            txns: HashMap::with_capacity(BLOCK_TXN_SIZE),
             timestamp: SystemTime::now(),
         }
     }
