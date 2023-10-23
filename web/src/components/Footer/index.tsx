6dba6400871545b385c349bb8c704152c5ff684c
vilayat-ali <vilayatcodemysite@gmail.com>
2023-10-23 18:20:58 +0530

    added layout component for website

---
+++ web/src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+// lib
+
+const Footer = () => {
+  return (
+    <div>
+      <div>footer</div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ web/src/components/Navbar/index.tsx
@@ -0,0 +1,25 @@
+// hooks
+import useToggle from "../../hooks/useToggle";
+
+// icons
+import { MdMenu, MdMenuOpen } from "react-icons/md";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="">
+        {/* Logo */}
+        <div className=""></div>
+        {/* Logo */}
+
+        {/* Desktop Menu */}
+        {/* Desktop Menu */}
+
+        {/* Hamburger Menu Icons */}
+        {/* Hamburger Menu Icons */}
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
