07e32df7468ca20f55a176c315510281b3edb5d9
vilayat-ali <vilayatcodemysite@gmail.com>
2023-10-28 14:07:51 +0530

    Fix: Added eventloop

---
+++ chain/src/node/src/eventloop/mod.rs
@@ -0,0 +1,23 @@
+use structure::txn::Txn;
+
+#[derive(Debug)]
+pub struct EventLoop {
+    pub tasks: Vec<Txn>,
+}
+
+impl EventLoop {
+    pub fn new() -> Self {
+        Self { tasks: Vec::new() }
+    }
+
+    pub fn task_enqueue(&mut self, txn: Txn) {
+        self.tasks.push(txn);
+    }
+
+    pub fn task_dequeue(&mut self) -> Option<Txn> {
+        match self.tasks.first() {
+            Some(_) => Some(self.tasks.remove(0)),
+            None => None,
+        }
+    }
+}
