23e87eb3cf57bd9b4bad03a951c2f8603371bd4d
vilayat-ali <vilayatcodemysite@gmail.com>
2023-11-16 00:36:45 +0530

    replaced cli based explorer with a react app

---
+++ chain/explorer/vite.config.ts
@@ -0,0 +1,13 @@
+// lib
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
