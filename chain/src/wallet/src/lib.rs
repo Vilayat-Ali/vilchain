e388929a2f378b3e65132b68a819a10293d3ad90
vilayat-ali <vilayatcodemysite@gmail.com>
2023-11-26 14:33:14 +0530

    refactored code for ratatui

--- chain/src/wallet/src/lib.rs
+++ chain/src/wallet/src/lib.rs
@@ -1,5 +1,4 @@
 pub mod actions;
 pub mod cred;
 pub mod filer;
-pub mod layouts;
-pub mod widgets;
+pub mod term;
---
+++ chain/src/wallet/src/term/mod.rs
@@ -0,0 +1,7 @@
+pub mod app;
+pub mod event;
+pub mod layouts;
+pub mod tui;
+pub mod ui;
+pub mod update;
+pub mod widgets;
