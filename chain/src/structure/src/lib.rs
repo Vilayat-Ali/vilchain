a99407ab3490e2ce1987b42da0906bf6c094ac62
vilayat-ali <vilayatcodemysite@gmail.com>
2024-02-07 23:09:55 +0530

    added node

--- chain/src/structure/src/lib.rs
+++ chain/src/structure/src/lib.rs
@@ -1,14 +1,43 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use serde::{Deserialize, Serialize};
+use std::fmt::{Debug, Display};
+
+#[derive(Serialize, Deserialize)]
+pub struct FloatValue {
+    pub int_val: u32,
+    pub zeroes: u8,
+    pub deci_val: u16,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl FloatValue {
+    pub fn new(int_val: u32, zeroes: u8, deci_val: u16) -> Self {
+        Self {
+            int_val,
+            zeroes,
+            deci_val,
+        }
+    }
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Display for FloatValue {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}.{}{}",
+            self.int_val,
+            "0".repeat(self.zeroes as usize),
+            self.deci_val
+        )
+    }
+}
+
+impl Debug for FloatValue {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}.{}{}",
+            self.int_val,
+            "0".repeat(self.zeroes as usize),
+            self.deci_val
+        )
     }
 }
