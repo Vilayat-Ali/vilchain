95e253570c7b12a28cd79b71bc583900e8221bd9
vilayat-ali <vilayatcodemysite@gmail.com>
2024-02-21 21:59:27 +0530

    MODIFIED: Wallet Credit Struct

--- chain/src/node/src/main.rs
+++ chain/src/node/src/main.rs
@@ -1,9 +1,4 @@
-use structure::BigNum;
-
-fn main() {
-        let mut b1 = BigNum::from_string("22.0000").unwrap();
-        let b2 = BigNum::from_string("11.0000").unwrap();
-      let mut b1_c = b1.clone();
-        b1_c.add(&b2);
-        println!("{:#?} - {:#?} = {:?}", b1, b2, b1_c);
+#[tokio::main]
+async fn main() {
+  println!("Hello World!");
 }
--- chain/src/wallet/src/cred.rs
+++ chain/src/wallet/src/cred.rs
@@ -1,40 +1,41 @@
 extern crate bip39;
 extern crate rand;
 
+use std::fmt::Debug;
+
 use bip39::{Mnemonic, Language, Error};
 use rand::{rngs::StdRng, Rng, SeedableRng};
-use rsa::{RsaPrivateKey, RsaPublicKey};
+use rsa::{pkcs8::EncodePublicKey, traits::PublicKeyParts, RsaPrivateKey, RsaPublicKey};
+use serde::{Serialize, Deserialize};
 
+#[derive(Debug)]
 pub struct WalletCred {
     pub wallet_address: String,
     mnemonic: Mnemonic,
     pub seed_words: Vec<String>,
-    private_key: Vec<u8>,
-    pub public_key: Vec<u8>
+    private_key: RsaPrivateKey,
+    pub public_key: RsaPublicKey
 }
 
 const BITS: usize = 2048;
 
 impl WalletCred {
     pub fn new() -> Result<WalletCred, Error> {
-    let mut rng = StdRng::from_entropy();
+        let mut rng = StdRng::from_entropy();
 
-    let private_key = RsaPrivateKey::new(&mut rand::thread_rng(), BITS).unwrap();
-    let public_key = RsaPublicKey::from(&private_key);
+        let private_key = RsaPrivateKey::new(&mut rand::thread_rng(), BITS).unwrap();
+        let public_key = RsaPublicKey::from(&private_key);
 
-    let entropy = (0..32).map(|_| rng.gen::<u8>()).collect::<Vec<u8>>();
-    let mnemonic = Mnemonic::from_entropy_in(Language::English, &entropy)?;
+        let entropy = (0..32).map(|_| rng.gen::<u8>()).collect::<Vec<u8>>();
+        let mnemonic = Mnemonic::from_entropy_in(Language::English, &entropy)?;
 
-    Ok(WalletCred {
-        wallet_address: String::default(),
-        mnemonic,
-        seed_words: Vec::default(),
-        private_key: Vec::default(),
-        public_key: Vec::default()
-    })
-}
-
-    pub fn get_seed_words(&self) -> Vec<String> {
-        (&self.mnemonic).to_string().split(' ').map(|x| x.trim().to_string()).collect::<Vec<String>>()
+        Ok(WalletCred {
+            wallet_address: format!("0xasd9034n3248324n32498234h9rfnwef98fy"),
+            mnemonic: mnemonic.clone(),
+            seed_words: mnemonic.to_string().split(' ').map(|x| x.trim().to_string()).collect::<Vec<String>>(),
+            private_key,
+            public_key
+        })
     }
+
 }
